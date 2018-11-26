085ef748a72f1b9a700e57259b7efecf8d5344ae
Sam Burns <sburns@scu.edu>
2018-11-26 14:54:07 -0800

    Add ability to approve/deny events

--- attendance/forms.py
+++ attendance/forms.py
@@ -1,3 +1,4 @@
+#Simran
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -12,7 +13,7 @@ class LoginForm(AuthenticationForm):
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
 
 
-class SignUpForm(UserCreationForm):
+"""class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
@@ -20,7 +21,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
-
+"""
 
 class EventForm(forms.ModelForm):
     class Meta:
